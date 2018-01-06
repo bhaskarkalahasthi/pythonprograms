3228810f14baafb0d0b29b40bb1d067c317efc63
bhaskarkalahasthi <bhaskarkalahasthi@gmail.com>
2018-01-06 13:06:37 +0530

    divisable sum pair algoritham in hacker rank.py

---
+++ divisable sum pair algoritham in hacker rank.py
@@ -0,0 +1,16 @@
+import sys
+
+def divisibleSumPairs(n, k, ar):
+    # Complete this function
+    count=0
+    for i in range(len(ar)):
+        for j in range(i+1,len(ar)):
+            if((ar[i]+ar[j])%k==0):
+                count=count+1
+    return count
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+ar = list(map(int, input().strip().split(' ')))
+result = divisibleSumPairs(n, k, ar)
+print(result)
