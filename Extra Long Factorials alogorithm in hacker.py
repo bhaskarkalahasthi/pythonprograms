d82e0616daa0c71f919df788e3492bc8c13f4a5d
bhaskarkalahasthi <bhaskarkalahasthi@gmail.com>
2018-01-19 21:30:49 +0530

    Extra Long Factorials alogirithm in hacker rank.py

---
+++ Extra Long Factorials alogorithm in hacker.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+import math
+
+def extraLongFactorials(n):
+    # Complete this function
+    fact=math.factorial(n)
+    print(fact)
+n = int(input().strip())
+extraLongFactorials(n)
